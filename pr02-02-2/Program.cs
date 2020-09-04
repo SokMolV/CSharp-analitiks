36fd240741844d6f71989c6a0262c004e471740b
Mariya <student@C239U27>
2020-09-04 14:58:48 +0300

    6

--- pr02-02-2/Program.cs
+++ pr02-02-2/Program.cs
@@ -6,7 +6,11 @@ namespace pr02_02_2
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      string s = "Microsoft .NET Framework 2 Application Development Foundation";
+      string[] sa = s.Split(" ");
+      Array.Sort(sa);
+      s = string.Join(" ", sa); 
+      Console.WriteLine(s);
     }
   }
 }
