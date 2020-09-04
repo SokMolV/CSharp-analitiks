c26ca0c2bbb833a6511675988fc38294b4d12c25
Mariya <student@C239U27>
2020-09-04 14:49:14 +0300

    5

--- pr01-01-1/Program.cs
+++ pr01-01-1/Program.cs
@@ -4,9 +4,24 @@ namespace pr01_01_1
 {
   class Program
   {
+    struct Person
+    {
+      public string firstName;
+      public string lastName;
+      public int age;
+      public Person(string _firstName, string _lastName, int _age)
+      {
+        firstName = _firstName;
+        lastName = _lastName; age = _age;
+      }
+      public override string ToString()
+      {
+        return firstName + " " + lastName + ", age " + age;
+      }
+    }
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      Person p = new Person("Tony", "Allen", 32); Console.WriteLine(p);
     }
   }
 }
---
+++ pr02-02-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace pr02_02_1
+{
+  class Program
+  {
+    SByte a = 0; 
+    Byte b = 0; 
+    Int16 с = 0; 
+    Int32 d = 0; 
+    Int64 e = 0; 
+    string s = "";
+    Exception ex = new Exception();
+    object[] types = { a, b, c, d, e, s, ex };
+
+    static void Main(string[] args)
+    {
+      foreach (object о in types)
+      {
+        string type;
+        if (o.GetType().IsValueType) type = "Value type";
+        else
+        type = "Reference Type";
+        Console.WriteLine("{0}: {1}", o.GetType(), type);
+      }
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
